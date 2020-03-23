5ec7ade7fea1bf23f1be478b1e6d7a432ceb6f7e
autumn-ragland <raglanda998@gmail.com>
2020-03-23 16:18:23 -0500

    graded

--- pokemon-react-app/src/AppContainer.jsx
+++ pokemon-react-app/src/AppContainer.jsx
@@ -3,28 +3,29 @@ import React, { Component } from 'react'
 export default class AppContainer extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-            dataList:[]
-             
+            dataList: [] // you are defining dataList as an array
+
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         console.log(`Component Did Mount`)
         this.loadData();
     }
-    loadData = async()=>{
+    loadData = async () => {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=900 ');
         const json = await response.json();
         console.log(json)
-        this.setState({dataList:json})
+        this.setState({ dataList: json }) // if you set dataList to `json.results` this works
     }
-    render() {console.log(this.state)
-
+    render() {
+        console.log(this.state)
+        // you can't access a property of an array
         return (
             <div>
-                {this.state.dataList.results.map((poke,index)=>{
-                    return(<div><p>{poke.name}</p></div>)
+                {this.state.dataList.results.map((poke, index) => {
+                    return (<div><p>{poke.name}</p></div>)
                 })}
             </div>
         )
